8f323ad54e3ba286d85e423097e3822975946695
Mohtashim <shoaibmohtashim973@gmail.com>
2024-04-28 13:08:26 +0000

    working with ot'

--- overtime/overtime/doctype/overtime/overtime.py
+++ overtime/overtime/doctype/overtime/overtime.py
@@ -2,19 +2,17 @@ import frappe
 from frappe.model.document import Document
 
 class Overtime(Document):
-    def refresh(self):
-        self.attendance_date =  self.get_attendance_date()
+    def overtime_hours(self):
+        worked_hours = self.out_time - self.in_time
+        total_seconds = total_seconds.total_seconds()
+        hours = total_seconds // 3600
+        minutes = (total_seconds % 3600) // 60
+        seconds = total_seconds % 60
+        print(hours,":",minutes,":","seconds")
+        print("Hours:", hours)
+        print("Minutes:", minutes)
+        print("Seconds:", seconds)    
+        print(worked_hours)
+        frappe.msgprint(worked_hours)
 
-    def get_attendance_date(self):
-        if not self.attendance_date:
-            return[]
-        attendance_records = frappe.get_all('Attendance',filters={'attendance_date':self.posting_date}, fields=['employee','check_in','check_out'])
-
-    def populate_attendance_detail(self):
-        if not self.attendance_date:
-            return
-        for record in self.attendance_records:
-            new_row = self.append('Attendance Detail')
-            new_row_employee = record.employee
-            new_row_check_in = record.check_in
-            new_row_check_out = record.check_out
+        
