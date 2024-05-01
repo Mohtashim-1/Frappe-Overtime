b67a8a3f16160de05d6ed3fd01f0a9d46b9b6220
Mohtashim <shoaibmohtashim973@gmail.com>
2024-05-01 20:20:22 +0000

    working on salary

--- overtime/overtime/doctype/overtime/overtime.py
+++ overtime/overtime/doctype/overtime/overtime.py
@@ -3,16 +3,20 @@ from frappe.model.document import Document
 
 class Overtime(Document):
     def before_save(self):
+        ctc = frappe.get_all('Employee',filters={'employee':self.employee},fields=['employee','ctc'])
         frappe.msgprint('Message')
         
-        self.ot_hours_calculation()
+
+    #     frappe.msgprint('Message')
+        
+    #     self.ot_hours_calculation()
 
 
-    def ot_hours_calculation(self):
-        # Calculate overtime hours
-        self.overtime_hours = self.working_hours - self.standard_hours
-        # Save the document 
-        self.save()
+    # def ot_hours_calculation(self):
+    #     # Calculate overtime hours
+    #     self.overtime_hours = self.working_hours - self.standard_hours
+    #     # Save the document 
+    #     self.save()
 
-        # Optionally, you can also display a message to the user
-        frappe.msgprint(f"Overtime hours calculated: {self.overtime_hours}")
+    #     # Optionally, you can also display a message to the user
+    #     frappe.msgprint(f"Overtime hours calculated: {self.overtime_hours}")
